0d8c9675d8f484a7d155e8e10dfdb6db00faee5f
David Figueiredo <150687060+G0DQwerty@users.noreply.github.com>
2024-04-02 13:33:13 +0100

    CharacterControllerandKeyboardInpute
    Made all the character controller and keyboard inpute steps

--- Assets/Scripts/Controler.cs
+++ Assets/Scripts/Controler.cs
@@ -7,14 +7,43 @@ public class Controler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Step 10
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Step 3
+        //float horizontal = Input.GetAxis("Horizontal");
+        //Debug.Log(horizontal);
+        //Vector2 position = transform.position;
+        //position.x = position.x + 0.1f * horizontal;
+        //transform.position = position;
+
+        //Step 9
+        //float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
+        //Vector2 position = transform.position;
+        //position.x = position.x + 0.1f * horizontal;
+        //position.y = position.y + 0.1f * vertical;
+        //transform.position = position;
+
+        //Step 10
+        //float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
+        //Vector2 position = transform.position;
+        //position.x = position.x + 0.1f * horizontal;
+        //position.y = position.y + 0.1f * vertical;
+        //transform.position = position;
+
+        //Step 11
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal * Time.deltaTime;
+        position.y = position.y + 0.1f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
